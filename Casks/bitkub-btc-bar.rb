ffe081c52fd20ddd88d2d95dac30d690d6f89534
Pisit <pisit@ppknr.com>
2026-09-20 11:03:07 +0700

    Update Homebrew Cask checksum for v1.0.0

--- Casks/bitkub-btc-bar.rb
+++ Casks/bitkub-btc-bar.rb
@@ -1,6 +1,6 @@
 cask "bitkub-btc-bar" do
   version "1.0.0"
-  sha256 :no_check
+  sha256 "7c0aecd51b4235ec2f44e528fe2b84f129bb372b3735f06f929092eb0c7e7a62"
 
   url "https://github.com/greenoxz/bitkub-btc-bar/releases/download/v#{version}/BitkubBtcBar.zip"
   name "Bitkub BTC Bar"
