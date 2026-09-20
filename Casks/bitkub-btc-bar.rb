be7489a00803d51da0f54b39a65f0f02f2f06055
Pisit <pisit@ppknr.com>
2026-09-20 11:10:50 +0700

    Bump Cask to v1.0.1

--- Casks/bitkub-btc-bar.rb
+++ Casks/bitkub-btc-bar.rb
@@ -1,6 +1,6 @@
 cask "bitkub-btc-bar" do
-  version "1.0.0"
-  sha256 "7c0aecd51b4235ec2f44e528fe2b84f129bb372b3735f06f929092eb0c7e7a62"
+  version "1.0.1"
+  sha256 "acb0a06b1202e515a38be7bb937c286e2951697bd7cf02c58e16fd8a8f774433"
 
   url "https://github.com/greenoxz/bitkub-btc-bar/releases/download/v#{version}/BitkubBtcBar.zip"
   name "Bitkub BTC Bar"
